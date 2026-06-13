8c136e481afbd50d3e4c5612b3b1123863b14942
the-realgabriel <theopgabrie@gmail.com>
2026-06-13 13:27:55 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
